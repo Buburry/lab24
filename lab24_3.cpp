94440e84103cf8abb1392e86c3d625f2e2416aa2
Buburry <Natapon7934@gmail.com>
2024-03-01 16:32:34 +0700

    24.3

--- lab24_3.cpp
+++ lab24_3.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
+#include<set>
 
 using namespace std;
 
 //Write function count() here.
+int count(int data[],int a) {
+	set<int> myset;
+
+	for(int i = 0; i<a; i++){
+	myset.insert(data[i]);
+	}
+
+	int num = 0;
+	set<int>::iterator i;
+	for(i = myset.begin(); i != myset.end(); i++){
+		num++;
+	}
+
+	return num;
+}
+
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
